589377b89de73f5cf39b8d889d707cc6fccc83e1
BrunaDev <dev.bruna.magalhaes@gmail.com>
2021-09-08 09:40:31 -0300

    Adicionando informações na conta corrente

---
+++ bank_oo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace bank_oo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaBruna = new ContaCorrente();
+            
+            contaDaBruna.titular = "Bruna";
+            contaDaBruna.agencia = 1234;
+            contaDaBruna.conta = 790;
+            contaDaBruna.saldo = 120;
+
+            ContaCorrente contaDaBarbara = new ContaCorrente();
+
+            contaDaBarbara.titular = "Barbara";
+            contaDaBarbara.agencia = 1234;
+            contaDaBarbara.conta = 789;
+            contaDaBarbara.saldo = 100.50;
+
+            Console.WriteLine("O titular da conta é: "+contaDaBruna.titular);
+            Console.WriteLine("O titular da conta é: "+contaDaBarbara.titular);
+        }
+    }
+}
