4e9ab51005da7f9134802abfbbc82fcdb5f931d0
BrunaDev <dev.bruna.magalhaes@gmail.com>
2021-09-15 09:37:39 -0300

    Adição dos métodos da conta corrente

--- bank_oo/ContaCorrente.cs
+++ bank_oo/ContaCorrente.cs
@@ -6,11 +6,37 @@ public class ContaCorrente {
 
     public ContaCorrente(){} //Boas praticas que devem sempre ser seguidas
 
-    public ContaCorrente(string titular, int agencia, int conta, double saldo)
+    public ContaCorrente(string contacorrente_titular, int contacorrente_agencia, int contacorrente_conta, double contacorrente_saldo)
     {
-        this.titular = titular;
-        this.agencia = agencia;
-        this.conta = conta;
-        this.saldo = saldo;
+        /*this.titular = titular;*/ //No lugar de this, podemos apenas mudar o nome da variável para uma não existente
+        titular = contacorrente_titular;
+        agencia = contacorrente_agencia;
+        conta = contacorrente_conta;
+        saldo = contacorrente_saldo;
+    }
+
+    public bool Sacar (double valor){ //o método precisa de um retorno
+        if (this.saldo < valor){
+            return false;
+        }
+        else {
+            this.saldo -= valor;
+            return true;
+        }
+    }
+
+    public void Depositar (double valor){ //o método não precisa de um retorno
+        this.saldo += valor;
+    }
+
+    public bool Transferir(double valor, ContaCorrente contaDestino){
+        if(this.saldo < valor){
+            return false;
+        }
+        else{
+            this.saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
     }
 }
--- bank_oo/Program.cs
+++ bank_oo/Program.cs
@@ -7,14 +7,27 @@ namespace bank_oo
         static void Main(string[] args)
         {
             ContaCorrente contaDaBruna = new ContaCorrente("Bruna", 1234, 790,120);
-
             ContaCorrente contaDaBarbara = new ContaCorrente("Barbara", 1234, 789, 100.50);
-
             ContaCorrente contaDoPedro = new ContaCorrente("Pedro", 1234, 777, 1050);
 
-            Console.WriteLine("O titular da conta é: "+contaDaBruna.titular);
+            /*Console.WriteLine("O titular da conta é: "+contaDaBruna.titular);
             Console.WriteLine("O titular da conta é: "+contaDaBarbara.titular);
-            Console.WriteLine("O titular da conta é: "+contaDoPedro.titular);
+            Console.WriteLine("O titular da conta é: "+contaDoPedro.titular);*/
+
+            //Saque
+            bool retorno = contaDaBruna.Sacar(50);
+            Console.WriteLine(retorno);
+            Console.WriteLine("O saldo da conta da Bruna é: " + contaDaBruna.saldo);
+            
+            //Deposito
+            contaDaBruna.Depositar(30);
+            Console.WriteLine("O saldo da conta da Bruna é: " + contaDaBruna.saldo);
+
+            //Trasferencia
+            contaDaBruna.Transferir(100, contaDoPedro);
+            Console.WriteLine("O saldo da conta da Bruna é: " + contaDaBruna.saldo);
+            Console.WriteLine("O saldo da conta do Pedro é: " + contaDoPedro.saldo);
         }
     }
+
 }
