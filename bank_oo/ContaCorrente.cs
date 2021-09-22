ca8b0404698445b024bbab8e28fe98bcdfb584c6
BrunaDev <dev.bruna.magalhaes@gmail.com>
2021-09-22 09:40:20 -0300

    Adicionando propriedades e mais umas funções

--- bank_oo/ContaCorrente.cs
+++ bank_oo/ContaCorrente.cs
@@ -1,37 +1,49 @@
 public class ContaCorrente {
 
-    public string titular;
-    public int agencia;
-    public int conta;
-    public double saldo {get; private set;}
+    public string Titular { get; set; }
+    public int Agencia { get; set; }
+    public int Conta { get; set; }
+
+    private double _saldo;
+
+    public double Saldo {
+            get{
+                return this._saldo;
+            }
+            set{
+                if(value >= 0){
+                    this._saldo = value;
+                }
+            }
+        }
 
     public ContaCorrente(){} //Boas praticas que devem sempre ser seguidas
 
-    public ContaCorrente(string contacorrente_titular, int contacorrente_agencia, int contacorrente_conta, double contacorrente_saldo)
+    public ContaCorrente(string contacorrente_titular, int contacorrente_agencia, double contacorrente_saldo)
     {
         /*this.titular = titular;*/ //No lugar de this, podemos apenas mudar o nome da variável para uma não existente
-        titular = contacorrente_titular;
-        agencia = contacorrente_agencia;
-        conta = contacorrente_conta;
-        saldo = contacorrente_saldo;
+        Titular = contacorrente_titular;
+        Agencia = contacorrente_agencia;
+        Saldo = contacorrente_saldo;
+
     }
 
     public bool Sacar (double valor){ //o método precisa de um retorno
-        if (this.saldo < valor){
+        if (this.Saldo < valor){
             return false;
         }
         else {
-            this.saldo -= valor;
+            this.Saldo -= valor;
             return true;
         }
     }
 
     public void Depositar (double valor){ //o método não precisa de um retorno
-        this.saldo += valor;
+        this.Saldo += valor;
     }
 
     public bool Transferir(double valor, ContaCorrente contaDestino){
-        if(this.saldo < valor){
+        if(this.Saldo < valor){
             return false;
         }
         else{
